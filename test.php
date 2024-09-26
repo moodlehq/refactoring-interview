e587115379a943e865b226320444892dbb9fbfd8
Mathew May <mathewm@hotmail.co.nz>
2024-09-26 10:48:43 +0800

    people enrolled

--- test.php
+++ test.php
@@ -8,14 +8,15 @@
 $schooldata = json_decode(file_get_contents('data.json'), true);
 
 $whoiswhere = enrolledIntoclass($schooldata);
-//print_r($whoiswhere);
+
+print_r($whoiswhere);
 
 function getlearnersintheSchool($schooldata, $switch = false) {
 
 
     foreach ($schooldata as $classes) {
         if ($classes[0]['classes'] and $switch == false) {return array('isAUserTHING' => true, 'users' => $classes);} else {
-            return ['isclassrominfo' => 'true', 'classes' => $classes];
+            return ['isclassrominfo' => 'true', 'classes' => $schooldata['classes']];
         }}return null;
 }
 
@@ -28,11 +29,19 @@ function enrolledIntoclass($schooldata) {
     $enrolments->isaclassrepresentation = 'yes';
     $leaners = getlearnersintheSchool($schooldata);
     $class = getSchoolClassInformation($schooldata);
-    foreach ($leaners['users'] as $key => $value) {
-        print_r($value);
-        foreach ($value['classes'] as $key => $class) {
-            //$enrolments->class = $class;
-            print_r($class);
+    foreach ($class['classes'] as $key => $value) {
+        $courseid = $value['id'];
+        $coursename = $value['name'];
+        $enrolments->$courseid = new stdClass();
+        $enrolments->$courseid->name = $coursename;
+        $enrolments->$courseid->students = [];
+
+        foreach ($leaners['users'] as $key => $value) {
+            foreach ($value['classes'] as $key => $class) {
+                if ($class['id'] == $courseid) {
+                    $enrolments->$courseid->students[] = $value['name'];
+                }
+            }
         }
     }
     return $enrolments;
