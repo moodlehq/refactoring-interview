68a79e4bc8399b6ef08528f6e478f58f9e2e3fd7
salman <salman@salmans-MacBook-Pro.local>
2024-10-03 23:58:54 +1000

    Now call getShoolPupils from userCRUD

--- school.php
+++ school.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'lib.php';
+require_once 'SchoolDataManager.php';
 
 $help = "Allows you to print out the entire school dataset currently in memory
 
@@ -10,5 +11,36 @@ Options:
 
 Example:
 \$ php school.php -a";
+
+// Assume $schoolData is loaded from a JSON file
+// $schoolData = returnData();
+
+// print_r($schoolData)
+
+// $schoolDataManager = new SchoolDataManager($schoolData);
+
+// Get all users
+// $users = $schoolDataManager->getUsers();
+// foreach ($users as $user) {
+    // print_r($user->getUserInfo());
+// }
+
+// Get all enrollments
+// $enrollments = $schoolDataManager->getEnrollments();
+// print_r($enrollments);
+// foreach ($enrollments as $enrollment) {
+//     print_r($enrollment->getEnrollmentInfo());
+// }
+
+// Find enrollment by course ID
+// $courseId = 1;  // Example course ID
+// $enrollment = $schoolDataManager->findEnrollmentByCourseId($courseId);
+
+// if ($enrollment) {
+//     echo "Enrollment for course ID $courseId:\n";
+//     print_r($enrollment->getEnrollmentInfo());
+// } else {
+//     echo "No enrollment found for course ID $courseId\n";
+// }
 
 print_r(printSchoolData());
