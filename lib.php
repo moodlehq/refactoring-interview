dc9a2aa90d5681d5b40e1f2eead457f21adea694
salman <salman@salmans-MacBook-Pro.local>
2024-10-03 23:46:28 +1000

    Remove now redundant function, as enrolments now come from another class

--- lib.php
+++ lib.php
@@ -29,40 +29,6 @@ function getSchoolClassInformation($dataoverride = null) {
     return getlearnersintheSchool($schooldata,true);
 }
 
-function enrolledIntoclass($dataoverride = null) {
-    if (!$dataoverride) {
-        $schooldata = returnData();
-    } else {
-        $schooldata = $dataoverride;
-    }
-    $enrolments = new stdClass();
-    $enrolments->isaclassrepresentation = 'yes';
-    $leaners = getlearnersintheSchool($schooldata, false);
-    $class = getSchoolClassInformation($schooldata);
-    foreach ($class['classes'] as $key => $value) {
-        $courseid = $value['id'];
-        $coursename = $value['name'];
-        $enrolments->$courseid = new stdClass();
-        $enrolments->$courseid->name = $coursename;
-        $enrolments->$courseid->students = [];
-
-        foreach ($leaners['users'] as $key => $value) {
-            foreach ($value['classes'] as $key => $class) {
-                if ($class['id'] == $courseid) {
-                    if (array_key_exists('role', $value) and $value['role'] == 'Teacher' and isset($value['email'])) {
-                        $enrolments->$courseid->teachers = $value['name'] . ': ' . $value['email'];
-                    } else {
-                        if (isset($value['email'])) {
-                            $enrolments->$courseid->students[] = $value['name'] . ': ' . $value['email'];
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return $enrolments;
-}
-
 function printSchoolData() {
     
     $schoolData = returnData();
